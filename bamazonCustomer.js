33c24ac61e65db5f737dd3bf593c00e8ebecb3b7
Paul Zavattieri <paulz92@gmail.com>
2017-12-15 12:40:25 -0500

    customer facing functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,11 @@
+// npm requirements
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 var consoleTable = require("console.table");
+// my password for mysql db connection - hidden
 var pw = require("./pw.js");
 
+// create mysql connection
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
@@ -11,8 +14,139 @@ var connection = mysql.createConnection({
 	database: "bamazon_db"
 });
 
+// connect to db
 connection.connect(function(error){
 	if (error) throw error;
-	console.log("conncted as id " + connection.threadId);
-	connection.end();
+	// welcome customer
+	console.log("\n-----------------------------------------------------------------" 
+		+ "\nWelcome to Bamazon! Check out what we've got for you!\n" 
+		+ "-----------------------------------------------------------------\n");
+	// start the app
+	welcome();
 });
+
+// initial screen upon app open
+function welcome() {
+	// ask customer what they'd like to do
+	inquirer.prompt([
+		{
+			name: "action",
+			type: "list",
+			choices: ["View items for sale", "Leave the store"],
+			message: "Please select what you would like to do."
+		}
+	]).then(function(action) {
+		// if user wants to view items, run the view items function
+		if (action.action === "View items for sale") {
+			viewItems();
+			// if user wants to leave, run exit function
+		} else if (action.action === "Leave the store") {
+			exit();
+		}
+	});
+}
+
+// welcome screen function on opening app
+function viewItems() {
+	// save my sql query
+	var query = "SELECT item_id, product_name, price, stock_quantity FROM products";
+	// query db display results
+	connection.query(query, function(error, results) {
+		// if error, tell us
+		if (error) throw error;
+		// display all items, id, price
+		console.log(" "); // space for node aesthetics
+		for (var i = 0; i < results.length; i++) {
+			console.log("Item ID: " + results[i].item_id + " | Item: " 
+				+ results[i].product_name + " | Price: $" + results[i].price);
+		}
+		console.log(" "); // space for node aesthetics
+		// inquire customer what they'd like to buy and how much qty
+		inquirer.prompt([
+			{
+				name: "id",
+				message: "Please enter the ID of the item that you would like to purchase.",
+				// validates that the id is a number greater than 0 and less than/equal to 
+				// the number of items
+				validate: function(value) {
+					if (value > 0 && isNaN(value) === false && value <= results.length) {
+						return true;
+					}
+					return false;
+				}
+			},
+			{
+				name: "qty",
+				message: "What quantity would you like to purchase?",
+				// validate the quantity is a number larger than 0
+				validate: function(value) {
+					if (value > 0 && isNaN(value) === false) {
+						return true;
+					}
+					return false;
+				}
+			}
+		]).then(function(transaction) {
+			// init itemQty, itemPrice, itemName vars
+			var itemQty;
+			var itemPrice;
+			var itemName;
+			// set above vars equal to results where the user id matches db id
+			for (var j = 0; j < results.length; j++) {
+				if (parseInt(transaction.id) === results[j].item_id) {
+					itemQty = results[j].stock_quantity;
+					itemPrice = results[j].price;
+					itemName = results[j].product_name;
+				}
+			}
+			// if user tries to buy more qty than db has available, tell them no, run the
+			// welcome function again
+			if (parseInt(transaction.qty) > itemQty) {
+				console.log("\nInsufficient inventory for your requested quantity!" 
+					+ " Try again.\n");
+				welcome();
+			} 
+			// if user tries to buy equal/less qty than db has available, tell them yes,
+			// update the database to reduce qty by customer purchase amt
+			else if (parseInt(transaction.qty) <= itemQty) {
+				console.log("\nCongrats! You successfully purchased " + transaction.qty 
+					+ " of " + itemName + ".");
+				lowerQty(transaction.id, transaction.qty, itemQty, itemPrice);
+			}
+		});
+	});
+}
+
+// reduce stock qty function
+function lowerQty(item, purchaseQty, stockQty, price) {
+	// query with an update, set stock equal to stockqty - purchase qty
+	// where the item_id equals the id the user entered
+	connection.query(
+		"UPDATE products SET ? WHERE ?", 
+		[
+			{stock_quantity: stockQty - parseInt(purchaseQty)},
+			{item_id: parseInt(item)}
+		],
+		// throw error if error, else run displayCost
+		function(error, response) {
+			if (error) throw error;
+			displayCost(purchaseQty, price);
+	});
+}
+
+// display total cost function
+function displayCost(purchaseQty, price) {
+	// save customer cost as variable - their qty * db price
+	var customerCost = parseInt(purchaseQty) * price;
+	// log it fixed to 2 decimals
+	console.log("The total price is $" + customerCost.toFixed(2) 
+		+ ". Gotta love no sales tax. Thanks for you purchase!\n");
+	// run welcome function
+	welcome();
+}
+
+// exit function says bye to user and ends db connection
+function exit() {
+	console.log("\nThanks for stopping by! Have a good day.");
+	connection.end();
+}
